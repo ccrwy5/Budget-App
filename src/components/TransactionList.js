7cd23dcce6cb997c5fd0c45e144ea69502880472
Chris Rehagen <ccrwy5@mail.missouri.edu>
2020-07-28 22:20:47 -0500

    Added delete and add functionality

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
+import {Transaction} from "./Transaction"
 
 export const TransactionList = () => {
   const { transactions } = useContext(GlobalContext);
@@ -8,11 +9,7 @@ export const TransactionList = () => {
     <div>
       <h3>History</h3>
       <ul id="list" className="list">
-        {transactions.map(transaction => (<li className="minus">
-                  {transaction.text} <span>-$400</span>
-                  <button className="delete-btn">x</button>
-                </li>
-        ))}
+        {transactions.map(transaction => ( <Transaction key={transaction.id} transaction={transaction}/>))}
 
       </ul>
     </div>
